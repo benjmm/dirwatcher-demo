74691faf7e231b7d0b56074f87ebeb2597ca743a
Benjamin Mark <benjmark@protonmail.com>
2020-03-28 14:34:40 -0400

    basic logging configured

---
+++ dirwatcher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import datetime
+
+logger = logging.getLogger(__file__)
+
+
+def main():
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s [%(threadName)-12s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logger.setLevel(logging.DEBUG)
+    app_start_time = datetime.datetime.now()
+    logger.info(
+        '\n'
+        '-------------------------------------------------------------------\n'
+        '    Running {0}\n'
+        '    Started on {1}\n'
+        '-------------------------------------------------------------------\n'
+        .format(__file__, app_start_time.isoformat())
+    )
+    uptime = datetime.datetime.now()-app_start_time
+    logger.info(
+        '\n'
+        '-------------------------------------------------------------------\n'
+        '    Stopped {0}\n'
+        '    Uptime was {1}\n'
+        '-------------------------------------------------------------------\n'
+        .format(__file__, str(uptime))
+    )
+
+
+if __name__ == '__main__':
+    main()
